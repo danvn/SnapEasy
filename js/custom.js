0a977aef3e03029cafddef8a5b58553be0db64bf
Alex-Luo <allu6345@colorado.edu>
2016-08-22 11:13:14 -0600

    stopped the autoplay on the carousel

--- js/custom.js
+++ js/custom.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
         paginationSpeed: 400,
         singleItem: true,
         transitionStyle: "fadeUp",
-        autoPlay: true,
+        autoPlay: false,
         navigationText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
 
     });
